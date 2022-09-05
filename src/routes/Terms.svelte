0dedb38986ec0d7ee147876d59275db1f8e2949d
Charlie Boalch <charlieboalch@gmail.com>
2022-09-05 12:05:41 -0400

    Actually add the terms module thingy

---
+++ src/routes/Terms.svelte
@@ -0,0 +1,46 @@
+<style>
+    div {
+        width: 100%;
+        text-align: center;
+    }
+
+    span {
+        display: inline-block;
+        text-align: center;
+        background-color: aliceblue;
+        font-size: large;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        margin: 0.2em;
+        margin-bottom: 1em;
+    }
+</style>
+
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    export let map;
+    if (!(map instanceof Map)) throw new TypeError("map is not instace of Map");
+
+    const keys = Array.from(map.entries());
+    keys.splice(keys.length - 1, 1)
+    console.log(keys.toString())
+
+    function returnHome(){
+        dispatch("message", {
+            text: 'home'
+        });
+    }
+</script>
+
+{#each keys as key}
+    <div>
+        <span>{key[0]}</span>
+        <span>{key[1]}</span>
+    </div>
+{/each}
+
+<div>
+    <button on:click={returnHome}>Home</button>
+</div>
